4544c1a9c6046eed61dc34833eab4bce5ac20510
njabuloszulu <njabuloszulu@gmail.com>
2019-08-15 16:57:02 +0200

    Extracting state logic to redux

---
+++ free-code-camp-challanges/React-and-Redux/Extract-state-logic-to-redux.js
@@ -0,0 +1,15 @@
+const ADD = 'ADD';
+
+const addMessage = (message) => ({
+    type: ADD,
+    message
+})
+
+function messageReducer (state = [],action){
+    if (action.type == ADD){
+        return [...state,action.message]
+    };
+    return state;
+}
+
+const store = Redux.createStore(messageReducer)
