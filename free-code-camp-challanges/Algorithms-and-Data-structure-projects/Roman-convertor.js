713b8604122f70d88fa459dd01a423b1c3b79464
njabuloszulu <njabuloszulu@gmail.com>
2019-10-21 17:08:20 +0200

    edited to convert to numerals

--- free-code-camp-challanges/Algorithms-and-Data-structure-projects/Roman-convertor.js
+++ free-code-camp-challanges/Algorithms-and-Data-structure-projects/Roman-convertor.js
@@ -1,13 +1,13 @@
-function romanize(num) {
+function romanize(str) {
     var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-    var romanized = ""
-    for (var i = 0; i < numbers.length; i++) {
-        while (numbers[i] <= num) {
-            romanized += roman[i]
-            num -= numbers[i]
+    var romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    var romanized = 0;
+    for (var i = 0; i <= numbers.length; i++) {
+        while (str.indexOf(romans[i]) === 0) {
+            romanized += numbers[i];
+            str = str.replace(romans[i], '');
         }
     }
     return romanized
 }
-console.log(romanize(9));
+console.log(romanize('I'));
